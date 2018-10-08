675c4f45c97515111b9e5ff24b47f40c1df265f2
nukeop <nukeop@gumblert.tech>
2018-10-08 14:17:51 +0200

    tmux macros, i3 bindings

--- keymap.c
+++ keymap.c
@@ -14,6 +14,26 @@
 
 #define _______ KC_TRNS
 
+#define I3_LEFT  LGUI(KC_LEFT)
+#define I3_RIGHT LGUI(KC_RIGHT)
+#define I3_UP    LGUI(KC_UP)
+#define I3_DOWN  LGUI(KC_DOWN)
+
+enum custom_keycodes {
+    TMUX_0 = SAFE_RANGE,
+    TMUX_1,
+    TMUX_2,
+    TMUX_3,
+    TMUX_4,
+    TMUX_5,
+    TMUX_6,
+    TMUX_7,
+    TMUX_8,
+    TMUX_9,
+    TMUX_NEW,
+    TMUX_CLOSE
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,-------------------.
@@ -31,13 +51,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------'
    */
 
-[_BL] = LAYOUT_numpad_6x4(
-  KC_ESC,   KC_TAB,   MO(_FL),   KC_BSPC, \
-  KC_NLCK,  KC_PSLS,  KC_PAST,   KC_PMNS, \
-  KC_P7,    KC_P8,    KC_P9,              \
-  KC_P4,    KC_P5,    KC_P6,     KC_PPLS, \
-  KC_P1,    KC_P2,    KC_P3,              \
-  KC_P0,              KC_PDOT,   KC_PENT),
+[_BL] = LAYOUT_ortho_6x4(
+  _______,    _______, MO(_FL), I3_RIGHT, \
+  TMUX_CLOSE, _______, I3_UP,   I3_DOWN,  \
+  TMUX_NEW,   _______, _______, I3_LEFT,  \
+  _______,    TMUX_9,  TMUX_6,  TMUX_3,   \
+  TMUX_0,     TMUX_8,  TMUX_5,  TMUX_2,   \
+  KC_ESC,     TMUX_7,  TMUX_4,  TMUX_1),
 
   /* Keymap _FL: Function Layer
    * ,-------------------.
@@ -69,23 +89,86 @@ const uint16_t PROGMEM fn_actions[] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-   switch (keycode) {
-      case BL_TOGG:
-         if (record->event.pressed) {
-            cospad_bl_led_togg();
-         }
-         return false;
-      case BL_ON:
-         if (record->event.pressed) {
-            cospad_bl_led_on();
-         }
-         return false;
-      case BL_OFF:
-         if(record->event.pressed) {
-            cospad_bl_led_off();
-         }
-         return false;
-      default:
-         return true;
-   }
+  switch (keycode) {
+  case BL_TOGG:
+    if (record->event.pressed) {
+      cospad_bl_led_togg();
+    }
+    return false;
+  case BL_ON:
+    if (record->event.pressed) {
+      cospad_bl_led_on();
+    }
+    return false;
+  case BL_OFF:
+    if(record->event.pressed) {
+      cospad_bl_led_off();
+    }
+    return false;
+
+    // tmux macros
+  case TMUX_0:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("b")"0");
+    }
+    return false;
+  case TMUX_1:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("b")"1");
+    }
+    return false;
+  case TMUX_2:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("b")"2");
+    }
+    return false;
+  case TMUX_3:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("b")"3");
+    }
+    return false;
+  case TMUX_4:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("b")"4");
+    }
+    return false;
+  case TMUX_5:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("b")"5");
+    }
+    return false;
+  case TMUX_6:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("b")"6");
+    }
+    return false;
+  case TMUX_7:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("b")"7");
+    }
+    return false;
+  case TMUX_8:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("b")"8");
+    }
+    return false;
+  case TMUX_9:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("b")"9");
+    }
+    return false;
+  case TMUX_NEW:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("b")"c");
+    }
+    return false;
+  case TMUX_CLOSE:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("b")"&y");
+    }
+    return false;
+    
+  default:
+    return true;
+  }
 }
